e187e8f8a38e924f65ee26783b5ef292a96090b3
polkov <dadkop@gmail.com>
2015-03-05 18:10:49 +0100

    Create ajajaja.js

---
+++ ajajaja.js
@@ -0,0 +1,40 @@
+//vytvorené na bot účely :D
+
+API.on(API.CHAT, chat);
+API.on(API.ADVANCE, advance);
+
+function advance(obj) {
+	$('#woot').click();
+}
+
+function chat(data) {
+	var msg = data.message;
+	var from = data.un;
+
+	if(from === "-Vitaly") {
+		if(msg === "!meh") {
+			$('#meh').click();
+		}
+		if(msg === "!woot") {
+			$('#woot').click();
+		}
+	}
+
+	if(from === "!Polkov") {
+		if(msg === "!meh") {
+			$('#meh').click();
+		}
+		if(msg === "!woot") {
+			$('#woot').click();
+		}
+	}
+
+	if(from === "♚ ҚRUㄕQUՏ ♚") {
+		if(msg === "!meh") {
+			$('#meh').click();
+		}
+		if(msg === "!woot") {
+			$('#woot').click();
+		}
+	}
+}
